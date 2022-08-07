ce05d8dd6c6245910dd73345ad20f686d3ad82f8
sglkc <31957516+sglkc@users.noreply.github.com>
2022-08-07 16:29:42 +0700

    ✨ add translation api for vercel

---
+++ api/translate.cjs
@@ -0,0 +1,24 @@
+import translate from '@vitalets/google-translate-api';
+
+export default async function handler(req, res) {
+  const { text, from = 'auto', json = false, to = 'en' } = req.body || req.query;
+
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTION');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json({
+      body: "OK"
+    });
+  }
+
+  if (!text) return res.status(400).json({
+    error: "You have to add 'text' to your query/request"
+  });
+
+  const result = await translate(text, { from, to });
+
+  if (json) return res.status(200).json(result);
+  return res.status(200).send(result.text);
+}
